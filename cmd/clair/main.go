0e8d9eb5f30ee96ad83bc319d2128e8c6fdb17b8
Irene Chen <irene.chen@windriver.com>
2019-04-03 14:23:00 -0400

    reintroduced ubuntu for regression testing purposes

--- cmd/clair/main.go
+++ cmd/clair/main.go
@@ -56,7 +56,7 @@ import (
 	//_ "github.com/coreos/clair/ext/vulnsrc/oracle"
 	_ "github.com/coreos/clair/ext/vulnsrc/centos"
 	//_ "github.com/coreos/clair/ext/vulnsrc/rhel"
-	//_ "github.com/coreos/clair/ext/vulnsrc/ubuntu"
+	_ "github.com/coreos/clair/ext/vulnsrc/ubuntu"
 )
 
 func waitForSignals(signals ...os.Signal) {
